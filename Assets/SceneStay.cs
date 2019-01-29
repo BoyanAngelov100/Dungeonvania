f194a8618f5aad083e05e123dbb0c5a856206e55
Boyan <thundah@abv.bg>
2019-01-29 19:49:16 +0200

    Final

---
+++ Assets/SceneStay.cs
@@ -0,0 +1,36 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneStay : MonoBehaviour {
+
+    int startingSceneIndex;
+
+    private void Awake()
+    {
+        int numSceneStay = FindObjectsOfType<SceneStay>().Length;
+        if (numSceneStay > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex != startingSceneIndex)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,104 @@
+﻿  using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(250f, 250f);
+
+    bool isAlive = true;
+
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    CapsuleCollider2D myBodyCollider2D;
+    BoxCollider2D feet;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBodyCollider2D = GetComponent<CapsuleCollider2D>();
+        feet = GetComponent<BoxCollider2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (!isAlive) { return; }  
+
+        Run();
+        Jump();
+        FlipSprite();
+        ClimbLadder();
+        Die();
+	}
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");  // mejdu -1 +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void ClimbLadder()
+    {
+        if (!feet.IsTouchingLayers(LayerMask.GetMask("Climbing"))) 
+        {
+            myAnimator.SetBool("Climbing", false );
+            return; 
+        }
+
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);
+        myRigidBody.velocity = climbVelocity;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("Climbing", playerHasVerticalSpeed);
+    }
+
+    private void Jump()
+
+    {
+        if (!feet.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        { return; }
+
+            if (CrossPlatformInputManager.GetButtonDown("Jump"))
+            {
+                Vector2 jumpVelocityToadd = new Vector2(0f, jumpSpeed);
+                myRigidBody.velocity += jumpVelocityToadd;
+            }
+
+        
+    }
+
+    private void Die()
+    {
+        if (myBodyCollider2D.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards")))
+        {
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
+            FindObjectOfType<GameSession>().ProcessPlayerDeath();
+        }
+    }
+
+    private void FlipSprite()
+
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+
+    }
+
+}
